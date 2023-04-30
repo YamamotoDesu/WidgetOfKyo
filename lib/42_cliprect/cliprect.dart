f86db087cb6cd849f31d6f9d5ee7119ed597130e
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-04-30 18:23:19 +0900

    ClipPath

--- lib/42_cliprect/cliprect.dart
+++ lib/42_cliprect/cliprect.dart
@@ -1,57 +1,22 @@
 import 'package:flutter/material.dart';
 
-class MyClipPath extends StatefulWidget {
-  const MyClipPath({Key? key}) : super(key: key);
+class MyClipRect extends StatelessWidget {
+  const MyClipRect({Key? key}) : super(key: key);
 
-  @override
-  State<MyClipPath> createState() => _MyClipPathState();
-}
-
-class _MyClipPathState extends State<MyClipPath> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ClipPath(
-        clipper: CustomClipPath(),
-        child: Container(
-          color: Colors.red,
-          height: 400,
-          child: const Center(
-            child: Text(
-              'Clip Path',
-              style: TextStyle(color: Colors.white, fontSize: 40),
-            ),
+      appBar: AppBar(title: const Text('Clip Rect')),
+      body: Center(
+        child: ClipRect(
+          child: Image.asset(
+            'assets/images/marvel_heroes/iron-man.jpeg',
+            height: 300,
+            width: 300,
+            fit: BoxFit.cover,
           ),
         ),
       ),
     );
-  }
-}
-
-class CustomClipPath extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    double w = size.width;
-    double h = size.height;
-
-    final path = Path();
-
-    // (0, 0) // 1. point
-    path.lineTo(0, h); // 2. Point
-    path.quadraticBezierTo(
-      w * 0.5,
-      h - 100,
-      w,
-      h,
-    ); // 3. Point
-    path.lineTo(w, 0); // 4. Point
-    path.close();
-
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return false;
   }
 }
